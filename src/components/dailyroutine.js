8a9fef95b858cc7b45c84475cc788e5e979b548b
manvitha-chinni <manvithachinni444@gmail.com>
2021-04-08 21:14:27 +0530

    dailyroutine, tasks routes added

---
+++ src/components/dailyroutine.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Dailyroutine = ()=>{
+    return (<>
+        Dailyroutine
+    </>);
+};
+export default Dailyroutine;
--- src/components/todoMain.js
+++ src/components/todoMain.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Analyze from './analyze';
-import Home from './home';
+import Dailyroutine from './dailyroutine';
+import Dashboard from './dashboard';
 import Login from './login';
 import NavBar from './navbar';
+import Tasks from './tasks';
 const TodoMain = () => {
     return ( 
         <>
             <NavBar/>
             <Switch>
-                <Route path="/home" component={Home}/>
+                <Route path="/dashboard" component={Dashboard}/>
                 <Route path="/analyze" component={Analyze}/>
                 <Route path="/search/:name" component={Login}/>
+                <Route path="/tasks" component={Tasks}/>
+                <Route path="/dailyroutine" component={Dailyroutine} />
             </Switch>
         </>
         
